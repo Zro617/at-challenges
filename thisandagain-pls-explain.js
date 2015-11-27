abaec53b2ece0ab731324fb67ef679c92bf0e38d
Zro <zropotato@gmail.com>
2015-11-27 17:49:21 -0600

    Update thisandagain-pls-explain.js

--- thisandagain-pls-explain.js
+++ thisandagain-pls-explain.js
@@ -2,12 +2,13 @@
 
 (function() {
   var thispost = prompt("Put topic or post URL here boi");
-  if (!post) return console.log(">:I");
-  var comment = "pls explain "+thispost;
+  if (!thispost) return console.log(">:I");
+  var comment = "pls explain -> "+thispost;
+  var csrf = document.cookie.match(/scratchcsrftoken=([0-9a-zA-Z]+)/)[1]; // thanks @liam48D
+  if (!csrf) return console.log("No CSRF token found");
   var xhr = new XMLHttpRequest();
-  xhr.open("POST","https://scratch.mit.edu/site-api/comments/user/Zro716/add/",true);
-  // TODO: Work on getting the CSRF token
-  // xhr.setRequestHeader("X-CSRF-Token",csrf_token_goes_here);
+  xhr.open("POST","https://scratch.mit.edu/site-api/comments/user/thisandagain/add/",true);
+  xhr.setRequestHeader("X-Csrf-Token",csrf);
   xhr.onload = function() {
     if (xhr.status == 200) alert("Comment sent. Hope thisandagain replies.");
     else alert("Something went wrong! *angry :package:*");
