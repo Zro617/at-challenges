69c9fbda105337ed0820b8767ce51f33273cd991
Zro <zropotato@gmail.com>
2015-12-02 10:39:18 -0600

    bugs fixed

--- thisandagain-pls-explain.user.js
+++ thisandagain-pls-explain.user.js
@@ -2,29 +2,11 @@
 // @name         thisandagain Summoner
 // @namespace    thisandagainPlsExplainThisPost
 // @version      1.0
-// @description  Adds a button to magically summon thisandagain about a post
+// @description  Adds a button to posts to magically summon thisandagain
 // @author       Zro716
 // @match        https://scratch.mit.edu/discuss/*
 // @grant        none
 // ==/UserScript==
-
-function Summon(user,postid) {
-  if(confirm("Are you sure you want to summon "+user+"?")){
-    var comment = "pls explain -> https://scratch.mit.edu/discuss/post/"+postid+"/";
-    var csrf = document.cookie.match(/scratchcsrftoken=([0-9a-zA-Z]+)/); // thanks @liam48D
-    if (!csrf) return alert("You need to be logged in.");
-    csrf = csrf[0];
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST","https://scratch.mit.edu/site-api/comments/user/"+user+"/add/",true);
-    xhr.setRequestHeader("X-CSRFToken",csrf);
-    xhr.setRequestHeader("X-Requested-With","XMLHttpRequest");
-    xhr.onload = function() {
-      if (xhr.status == 200) alert(user + " has been summoned!");
-      else alert("Oops! Can't do that. :/");
-    };
-    xhr.send(JSON.stringify({content:comment, parent_id:"", commentee_id:""}));
-  }
-}
 
 // the only textarea is the markitup editor which is only shown if the user is logged in
 var isloggedin = document.getElementsByTagName("textarea"); 
@@ -38,7 +20,17 @@ if (posts.length) {
     //plsexplain.appendChild(document.createTextNode("| "));
     btn = document.createElement("button");
     btn.setAttribute("class","plsexplain");
-    btn.setAttribute("onclick","Summon('thisandagain',this.id)");
+    btn.setAttribute("onclick",
+      'if(confirm("Are you sure you want to summon thisandagain? Remember, please do not use this to spam him.")){'+
+      'var csrf=document.cookie.match(/scratchcsrftoken=([0-9a-zA-Z]+)/);'+
+      'if(!csrf)return alert("You need to be logged in.");'+
+      'csrf=csrf[1];var cmt="pls explain -> https://scratch.mit.edu/discuss/post/"+this.id+"/";'+
+      'var xhr = new XMLHttpRequest();'+
+      'xhr.open("POST","https://scratch.mit.edu/site-api/comments/user/thisandagain/add/",true);'+
+      'xhr.setRequestHeader("X-CSRFToken",csrf),xhr.setRequestHeader("X-Requested-With","XMLHttpRequest");'+
+      'xhr.onload=function(){alert(xhr.status==200?"thisandagain has been summoned!":"Oops! Cannot do that.");};'+
+      'xhr.send(JSON.stringify({content:cmt, parent_id:"", commentee_id:""}));}'
+    );
     btn.innerHTML = "thisandagain pls explain";
     id = posts[p].id.match(/\d+/)[0];
     btn.setAttribute("id",id);
